4dfabe401d8d8d66e14da77292865cc3fb918505
Guitarjun <arjunsrivastava01@gmail.com>
2020-08-20 14:28:53 -0700

    implemented Scraper.py; stale element exception

--- Scraper.py
+++ Scraper.py
@@ -1,3 +1,50 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import requests, time, random
+from selenium.common.exceptions import WebDriverException
+from bs4 import BeautifulSoup
+# TODO: IMPLEMENT SALES NAV
+
 class Scraper:
     def __init__(self, company_name: str, results: int, keywords: list, to_ignore: list):
-        pass
+        self.company_name = company_name
+        self.results = results
+        self.keywords = keywords
+        self.to_ignore = to_ignore
+
+        # Setting up browser
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    def run(self):
+        # Get login page
+        self.browser.get('https://www.linkedin.com/uas/login')
+
+        # Retrieves username and password from config.txt
+        config = open('config.txt')
+        lines = config.readlines()
+        username = lines[1].split(':')[1]
+        password = lines[2].split(':')[1]
+        config.close()
+
+        # Submits username and password keys
+        element = self.browser.find_element_by_id('username')  # Using username input id
+        element.send_keys(username)
+        element = self.browser.find_element_by_id('password')  # Using password input id
+        element.send_keys(password)
+
+        element.submit()
+
+        self.search()
+
+        self.browser.quit()  # Close Chrome browser
+
+    def search(self):
+        # Initial search
+        search = self.browser.find_element_by_xpath("//input[@aria-label='Search']")  # Using search bar xpath
+        search.send_keys(self.company_name)
+        self.browser.find_element_by_css_selector(
+            "button[class='search-global-typeahead__button']").click()  # Using search BUTTON (not bar itself) class name
+        time.sleep(5)
+        result = self.browser.find_element_by_id('ember9183')  # Finds first search result (MAKE MORE EFFECTIVE)
+        link = result.get_attribute('href')
+        self.browser.get(link)
--- main.py
+++ main.py
@@ -1,42 +1,9 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import requests, time, random
-from bs4 import BeautifulSoup
-# TODO: Add Sales Navigator Functionality?
+from Scraper import Scraper
 
 
 def main():
-    # Setting up browser
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.get('https://www.linkedin.com/uas/login')
-
-    # Retrieves username and password from config.txt
-    config = open('config.txt')
-    lines = config.readlines()
-    username = lines[1].split(':')[1]
-    password = lines[2].split(':')[1]
-
-    # Submits username and password keys
-    element_id = browser.find_element_by_id('username')  # Using username input id
-    element_id.send_keys(username)
-    element_id = browser.find_element_by_id('password')  # Using password input id
-    element_id.send_keys(password)
-    element_id.submit()
-
-    # Initial search
-    search = browser.find_element_by_xpath("//input[@aria-label='Search']")  # Using search bar xpath
-    search.send_keys('google')
-    browser.find_element_by_css_selector("button[class='search-global-typeahead__button']").click()  # Using search BUTTON (not bar itself) class name
-    time.sleep(5)
-    result = browser.find_element_by_id('ember9183')  # Finds first search result (MAKE MORE EFFECTIVE)
-    link = result.get_attribute('href')
-    browser.get(link)
-
-
-
-
-    # TODO: implement Scraper class, multi-query, etc.
-
+    scraper = Scraper('google', 10, [], [])
+    scraper.run()
 
 
 if __name__ == '__main__':
