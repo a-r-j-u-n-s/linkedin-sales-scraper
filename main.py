621dfaa5a6ccbcc86ada6809a43dbbd74bbe186d
Guitarjun <arjunsrivastava01@gmail.com>
2020-08-20 02:33:20 -0700

    add profile scrape

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 
 
 def main():
-
     # Setting up browser
     browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.get('https://www.linkedin.com/uas/login')
@@ -26,8 +25,14 @@ def main():
     # Initial search
     search = browser.find_element_by_xpath("//input[@aria-label='Search']")  # Using search bar xpath
     search.send_keys('Gabrielle Sprunck')
-    browser.find_element_by_css_selector("button[class='search-global-typeahead__button']").click()  # Using search button class name
+    browser.find_element_by_css_selector("button[class='search-global-typeahead__button']").click()  # Using search BUTTON (not bar itself) class name
     time.sleep(10)
+
+    # TODO: implement Scraper class, multi-query, etc.
+
+    # Get info from given link
+    link = 'https://www.linkedin.com/in/arjun-srivastava042701/'
+    browser.get(link)
 
 
 if __name__ == '__main__':
