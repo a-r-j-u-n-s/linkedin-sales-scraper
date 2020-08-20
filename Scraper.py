10a14fbcd8b155e33d190d72bfa23e47297e8bc8
Guitarjun <arjunsrivastava01@gmail.com>
2020-08-20 02:04:33 -0700

    created Scraper.py

---
+++ Scraper.py
@@ -0,0 +1,3 @@
+class Scraper:
+    def __init__(self, company_name: str, results: int, keywords: list, to_ignore: list):
+        pass
--- main.py
+++ main.py
@@ -6,22 +6,27 @@ from bs4 import BeautifulSoup
 
 def main():
 
-    # FIX PATH
+    # FIX PATH: todo for chromedriver?
     browser = webdriver.Chrome(ChromeDriverManager().install())
 
+    # Retrieves username and password
     browser.get('https://www.linkedin.com/uas/login')
     config = open('config.txt')
     lines = config.readlines()
-    username = lines[0].split()[1]
-    password = lines[1].split()[1]
+    username = lines[1].split()[1]
+    password = lines[2].split()[1]
 
+    # Submits username and password keys
     element_id = browser.find_element_by_id('username')
     element_id.send_keys(username)
-
     element_id = browser.find_element_by_id('password')
     element_id.send_keys(password)
-
     element_id.submit()
+
+    # Search functionality
+    search = browser.find_element_by_xpath("//input[@aria-label='Search']")
+    search.send_keys('apple')
+    search.submit()
 
 
 if __name__ == '__main__':
